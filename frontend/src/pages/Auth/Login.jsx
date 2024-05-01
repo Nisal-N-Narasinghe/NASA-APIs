e02548ab8a9774e2b3daf6879d14d43f81711d70
Nisal <it21259302@my.sliit.lk>
2024-05-01 21:16:57 +0530

    project transfer

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -25,7 +25,7 @@ const Login = () => {
 
     try {
       const response = await axios.post(
-        "http://127.0.0.1:5000/api/v1/auth/login",
+        "https://nasa-apis-server.vercel.app/api/v1/auth/login",
         formData
       );
 
