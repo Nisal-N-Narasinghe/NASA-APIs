54e2904c85f24a23a62906a0a6880ba02225066c
Nisal <it21259302@my.sliit.lk>
2024-05-01 21:22:35 +0530

    project transfer

--- server/app.js
+++ server/app.js
@@ -19,13 +19,6 @@ const cors = require("cors");
 
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "React app URL");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
-
 //as a good practice, at the top we should use helmet to secure our app
 app.use(helmet());
 
