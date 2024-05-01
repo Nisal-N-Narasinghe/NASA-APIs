ceb1d3453156451b46a5cb62f8586b30964730ba
Nisal <it21259302@my.sliit.lk>
2024-05-01 22:12:18 +0530

    vercel

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -24,7 +24,10 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await axios.post("/api/v1/auth/login", formData);
+      const response = await axios.post(
+        "https://nasa-apis-server.vercel.app/api/v1/auth/login",
+        formData
+      );
 
       console.log("Login successful:", response.data);
 
